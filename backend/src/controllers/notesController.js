61856d2679d0a3d91e3d119c22fc415b43ddfdaf
Alessandro Jablonski <34753426+ale2502@users.noreply.github.com>
2025-10-06 22:36:32 -0300

    middleware example

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -1,8 +1,8 @@
 import Note from '../models/Note.js'
 
-export async function getAllNotes(req, res) {
+export async function getAllNotes(_, res) { // Convention "_" when we don't use that parameter (not mandatory)
   try {
-    const notes = await Note.find();
+    const notes = await Note.find().sort({createdAt: -1}); // -1 will sort in desc. order (newest first)
     res.status(200).json(notes);
   } catch (error) {
     console.error('Error in getAllNotes controller', error);
@@ -13,6 +13,7 @@ export async function getAllNotes(req, res) {
 export async function getNoteById(req, res) {
   try {
     const note = await Note.findById(req.params.id);
+    if (!note) return res.status(404).json({message:'Note not found'});
     res.status(200).json(note);
   } catch (error) {
     console.error('Error in getNoteById controller', error);
--- backend/src/server.js
+++ backend/src/server.js
@@ -11,7 +11,13 @@ const PORT = process.env.PORT || 5001;
 connectDB();
 
 // middleware
-app.use(express.json());
+app.use(express.json()); // This middleware will parse JSON bodies: req.body
+
+// Ex. on how middlewares work
+// app.use((req, res, next) => {
+//   console.log(`Req method is ${req.method}, Req URL is ${req.url} and req body is ${JSON.stringify(req.body)}`);
+//   next();
+// });
 
 app.use('/api/notes', notesRoutes);
 
