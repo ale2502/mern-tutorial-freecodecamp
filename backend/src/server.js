f309ecd475c2b54f054f6fb1e41ae200ddfb3653
Alessandro Jablonski <34753426+ale2502@users.noreply.github.com>
2025-10-09 21:49:29 -0300

    setting ratelimiter from upstash

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,10 +14,10 @@ connectDB();
 app.use(express.json()); // This middleware will parse JSON bodies: req.body
 
 // Ex. on how middlewares work
-// app.use((req, res, next) => {
-//   console.log(`Req method is ${req.method}, Req URL is ${req.url} and req body is ${JSON.stringify(req.body)}`);
-//   next();
-// });
+app.use((req, res, next) => {
+  console.log(`Req method is ${req.method}, Req URL is ${req.url} and req body is ${JSON.stringify(req.body.title)}`);
+  next();
+});
 
 app.use('/api/notes', notesRoutes);
 
