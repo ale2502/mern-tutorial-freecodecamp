a6aca9b318fce0ed173e706d5e0f661209c64bf0
Alessandro Jablonski <34753426+ale2502@users.noreply.github.com>
2025-10-09 22:33:17 -0300

    latelimiter.js file

---
+++ backend/src/middleware/rateLimiter.js
@@ -0,0 +1,11 @@
+import ratelimit from '../config/upstash.js';
+
+const rateLimiter = async (req, res, next) => {
+  try {
+    const {success} = await ratelimit.limit()
+  } catch (error) {
+
+  }
+}
+
+export default rateLimiter
