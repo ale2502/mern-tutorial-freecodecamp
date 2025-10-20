9f9f42dcb17b802748c13fc7fe1040cd6ceea2dd
Alessandro Jablonski <34753426+ale2502@users.noreply.github.com>
2025-10-20 14:47:16 -0300

    adding navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,14 +6,7 @@ import toast from 'react-hot-toast';
 
 const App = () => {
   return (
-    <div>
-      <button className="btn">Button</button>
-      <button className="btn btn-neutral">Neutral</button>
-      <button className="btn btn-primary">Primary</button>
-      <button className="btn btn-secondary">Secondary</button>
-      <button className="btn btn-accent">Accent</button>
-      <button className="btn btn-ghost">Ghost</button>
-      <button className="btn btn-link">Link</button>
+    <div data-theme="forest">
       <Routes>
         <Route path='/' element={<HomePage/>}/>
         <Route path='/create' element={<CreatePage/>}/>
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,4 +1,7 @@
+import Navbar from "../components/Navbar";
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return <div className="min-h-screen">HomePage</div>;
+    <Navbar/>
 };
 export default HomePage
